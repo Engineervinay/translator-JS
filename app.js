f90310fd3b5b5abf1b097a744100085409d928af
engineervinay <vinay1645.vp@gmail.com>
2020-12-20 18:46:57 +0530

    input output completed

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var name= te;
+var btnTranslate=document.querySelector("#btn");
+var inText=document.querySelector("#inputText");
+var outText=document.querySelector("#outputText");
+
+function clickEventListener(){
+    outText.innerText=" hello " + inText.value;
+  
+}
+btnTranslate.addEventListener("click",clickEventListener);
+
+//console.log(btnTranslate);
