134534ef96fcafa1699c4977e8b5d0bc086096b4
engineervinay <vinay1645.vp@gmail.com>
2020-12-20 20:16:10 +0530

    completed js

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ var btnTranslate=document.querySelector("#btn");
 var inText=document.querySelector("#inputText");
 var outText=document.querySelector("#outputText");
 
+function errorHandler(error)
+{
+    console.log("you have error:" , error)
+    alert("maximum limit reach for today!! try again after 1 hour")
+}
 function clickEventListener(){
-    outText.innerText=" hello " + inText.value;
-  
+    
+    url="https://api.funtranslations.com/translate/leetspeak.json?text="+inText.value;
+    fetch(url)
+    .then (response=>response.json())
+    .then(json=>outText.innerText=json.contents.translated)
+    .catch(errorHandler)
+
+
 }
 btnTranslate.addEventListener("click",clickEventListener);
 
